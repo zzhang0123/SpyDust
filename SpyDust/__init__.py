72af7a94fc9dc4d75d6a89a54b93d089b5547ee2
Zheng Zhang <zheng_zhang1@alumni.brown.edu>
2025-01-04 16:42:56 +0000

    Retreived the missing files from the previous commit

--- SpyDust/__init__.py
+++ SpyDust/__init__.py
@@ -1,7 +1,7 @@
 # Initialisation file for the 'SpyDust' package.
 
 # Initialize package variables
-package_version = "1.0"
+package_version = "1.1"
 
 # Importing submodules for ease of access
 import os
