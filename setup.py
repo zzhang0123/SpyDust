f40360947f034f6892d6304bb5ec4dda23c68c5e
Zheng Zhang <zheng_zhang1@alumni.brown.edu>
2025-01-04 16:50:43 +0000

    fixed version tag

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="SpyDust",  # Required
-    version="1.0.1",  # Required
+    version="1.1",  # Required
     description="A code for modeling spinning dust radiation",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
